004408a6ce0e7a2cc6416531002104d7e6cce5e3
Diego Tavares Brazil <braziltga@gmail.com>
2019-02-20 18:39:01 -0300

    Implementação da tela panorâmica

---
+++ src/main/java/br/com/dbrazil/ccaixa/controller/PanoramicoController.java
@@ -0,0 +1,113 @@
+package br.com.dbrazil.ccaixa.controller;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import br.com.dbrazil.ccaixa.entidade.Caixa;
+import br.com.dbrazil.ccaixa.entidade.Movimentacao;
+import br.com.dbrazil.ccaixa.neg.MovimentacaoNeg;
+import br.com.dbrazil.ccaixa.util.faces.GenericFaces;
+
+@ManagedBean
+@ViewScoped
+public class PanoramicoController extends GenericFaces {
+
+	private static final long serialVersionUID = 1L;
+
+	@ManagedProperty("#{movimentacaoNeg}")
+	private MovimentacaoNeg movimentacaoNeg;
+
+	private Integer ano;
+	private Integer mes;
+	private Caixa caixa;
+	private List<Caixa> listaCaixa;
+	private Integer entradasMes;
+	private Integer saidasMes;
+	private Integer saldoMes;
+	private Integer entradass;
+	private Integer saidas;
+	private Integer saldo;
+	private List<Movimentacao> movimentacoesMes;
+
+	public PanoramicoController() {
+		super();
+	}
+
+	public List<Movimentacao> listar() {
+		return movimentacaoNeg.listaTudo();
+	}
+
+	/**
+	 * @return the ano
+	 */
+	public Integer getAno() {
+		return ano;
+	}
+
+	/**
+	 * @param ano the ano to set
+	 */
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	/**
+	 * @return the mes
+	 */
+	public Integer getMes() {
+		return mes;
+	}
+
+	/**
+	 * @param mes the mes to set
+	 */
+	public void setMes(Integer mes) {
+		this.mes = mes;
+	}
+
+	/**
+	 * @return the caixa
+	 */
+	public Caixa getCaixa() {
+		return caixa;
+	}
+
+	/**
+	 * @param caixa the caixa to set
+	 */
+	public void setCaixa(Caixa caixa) {
+		this.caixa = caixa;
+	}
+
+	/**
+	 * @return the movimentacoesMes
+	 */
+	public List<Movimentacao> getMovimentacoesMes() {
+		return movimentacoesMes;
+	}
+
+	/**
+	 * @param movimentacoesMes the movimentacoesMes to set
+	 */
+	public void setMovimentacoesMes(List<Movimentacao> movimentacoesMes) {
+		this.movimentacoesMes = movimentacoesMes;
+	}
+
+	/**
+	 * @return the movimentacaoNeg
+	 */
+	public MovimentacaoNeg getMovimentacaoNeg() {
+		return movimentacaoNeg;
+	}
+
+	/**
+	 * @param movimentacaoNeg the movimentacaoNeg to set
+	 */
+	public void setMovimentacaoNeg(MovimentacaoNeg movimentacaoNeg) {
+		this.movimentacaoNeg = movimentacaoNeg;
+	}
+
+}
